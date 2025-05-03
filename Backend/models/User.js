169a9e662351e4a3cc5b1d7790db47b176bbfa7b
natannekip <natashakippur@gmail.com>
2025-05-03 16:16:45 -0600

    Fixed syntax error

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-    theme: { type: String, default: 'light'},
-    notifications: { type: Boolean, default: true},
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  theme: { type: String, default: 'light' },
+  notifications: { type: Boolean, default: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
