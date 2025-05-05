215982c23cfc46f989899cafc60335c5c5d6e4c7
natkip <natasha.kippur@ucdenver.edu>
2025-05-04 21:23:12 -0600

    Update Task.js

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -5,8 +5,8 @@ const TaskSchema = new mongoose.Schema({
     description: String,
     status: {
       type: String,
-      enum: ['To Do', 'In Progress', 'Completed'],
-      default: 'To Do', // ✅ ADD THIS DEFAULT
+      enum: ['To Do', 'In Progress', 'Completed'], //Different task status
+      default: 'To Do', // This is the default 
     },
     userId: String,
   });
